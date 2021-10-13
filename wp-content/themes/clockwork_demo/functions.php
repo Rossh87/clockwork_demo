b59d6616562c8340acbad2acd23042557d2f8a53
Rossh87 <hunter.dev.87@gmail.com>
2021-10-13 16:00:27 -0500

    [theme]: add theme support for post thumbnails

--- wp-content/themes/clockwork_demo/functions.php
+++ wp-content/themes/clockwork_demo/functions.php
@@ -1,4 +1,5 @@
 <?php
 require_once(__DIR__ . '/includes/load-page-resources.php');
 require_once(__DIR__ . '/includes/setup-custom-header.php');
-require_once(__DIR__ . '/includes/setup_nav_menu.php');
+require_once(__DIR__ . '/includes/setup-nav-menu.php');
+require_once(__DIR__ . '/includes/setup-posts.php');
