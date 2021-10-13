617d4dfb027d3143264ca5ac93e6d793868956c0
Rossh87 <hunter.dev.87@gmail.com>
2021-10-13 17:22:25 -0500

    [theme]: add metadata to blog card UI

--- wp-content/themes/clockwork_demo/template-parts/template-blog_card.php
+++ wp-content/themes/clockwork_demo/template-parts/template-blog_card.php
@@ -15,8 +15,13 @@
 				?>
 			</div>
 			<div class="card-body">
+				<header class="d-flex justify-content-start mb-3">
+					<span aria-labelledby="blog-card-header-<?php get_the_ID(); ?>" aria-role="img" style='background-image: url("<?php get_the_post_thumbnail_url(); ?>");' alt="Blog post author" class="cd_avatar me-3"></span>
+					<small id="blog-card-header-<?php get_the_ID(); ?>" class="text-black-50 pt-3 text-center"><?php echo get_the_author(); ?></small>
+				</header>
 				<h5 class="card-title pb-3"><?php the_title(); ?></h5>
 				<p class="card-text"><?php the_excerpt(); ?></p>
+				<footer class="card-text"><small class="text-muted">Published <?php echo get_the_date(); ?></small></footer>
 			</div>
 		</div>
 	</a>
